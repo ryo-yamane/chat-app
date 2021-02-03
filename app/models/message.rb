049adcf9bedf6c6740b15ad98943898888469393
ryo-yamane <78146211+ryo-yamane@users.noreply.github.com>
2021-02-03 14:27:42 +0900

    画像表示の実装

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
-belongs_to :user
-belongs_to :room
-has_one_attached :image
+  belongs_to :user
+  belongs_to :room
+  has_one_attached :image
 
-validates :content, presence: true
+  validates :content, presence: true
 end
